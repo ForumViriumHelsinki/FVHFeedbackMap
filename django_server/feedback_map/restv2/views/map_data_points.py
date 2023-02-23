99f5b777cfa4336dd2dad56c5f4e20612d4a8013
Aapo (f021b) Rista <aapris@gmail.com>
2023-02-23 16:15:59 +0200

    Add pagination. Also run pre-commit hooks (black, ruff etc)

--- django_server/feedback_map/restv2/views/map_data_points.py
+++ django_server/feedback_map/restv2/views/map_data_points.py
@@ -1,45 +1,62 @@
-from django.db.models import Count, Q
 from django.utils import timezone
 from rest_framework import viewsets, permissions
 from rest_framework.decorators import action
 from rest_framework.generics import ListAPIView
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 from feedback_map import models
-from feedback_map.rest.permissions import IsReviewerOrCreator, IsReviewer, user_is_reviewer
-from feedback_map.rest.serializers import DictMapDataPointSerializer, MapDataPointSerializer, \
-    MapDataPointCommentSerializer, MapDataPointCommentNotificationSerializer, TagSerializer
+from feedback_map.rest.permissions import (
+    IsReviewerOrCreator,
+    IsReviewer,
+    user_is_reviewer,
+)
+from feedback_map.rest.serializers import (
+    DictMapDataPointSerializer,
+    MapDataPointSerializer,
+    MapDataPointCommentSerializer,
+    MapDataPointCommentNotificationSerializer,
+    TagSerializer,
+)
+
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 100
+    page_size_query_param = "page_size"
+    max_page_size = 1000
 
 
 class TagsViewSet(viewsets.ReadOnlyModelViewSet):
     permission_classes = [permissions.AllowAny]
     serializer_class = TagSerializer
-    queryset = models.Tag.objects.filter(published__isnull=False).order_by('button_position')
+    pagination_class = StandardResultsSetPagination
+    queryset = models.Tag.objects.filter(published__isnull=False).order_by(
+        "button_position"
+    )
 
 
 class MapDataPointsViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.AllowAny]
     serializer_class = MapDataPointSerializer
-    queryset = models.MapDataPoint.objects.filter(visible=True)
+    pagination_class = StandardResultsSetPagination
+    queryset = models.MapDataPoint.objects.filter(visible=True).order_by("-created_at")
 
     # Use simple serializer for list to improve performance:
-    serializer_classes = {
-        'list': DictMapDataPointSerializer
-    }
+    serializer_classes = {"list": DictMapDataPointSerializer}
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        if self.action == 'list':
+        if self.action == "list":
             # Fetch list as dicts rather than object instances for a bit more speed:
             return queryset.values()
         return queryset
 
     def get_permissions(self):
-        if self.action in ['update', 'partial_update', 'hide_note']:
+        if self.action in ["update", "partial_update", "hide_note"]:
             return [IsReviewerOrCreator()]
-        elif self.action in ['upvote', 'downvote']:
+        elif self.action in ["upvote", "downvote"]:
             return [permissions.IsAuthenticated()]
-        elif self.action == 'mark_processed':
+        elif self.action == "mark_processed":
             return [IsReviewer()]
         else:
             return super().get_permissions()
@@ -60,45 +77,46 @@ class MapDataPointsViewSet(viewsets.ModelViewSet):
             map_data_point.modified_by = self.request.user
         map_data_point.save()
 
-    @action(methods=['PUT'], detail=True)
+    @action(methods=["PUT"], detail=True)
     def mark_processed(self, request, *args, **kwargs):
         map_data_point = self.get_object()
         map_data_point.processed_by = request.user
         map_data_point.save()
-        return Response('OK')
+        return Response("OK")
 
-    @action(methods=['PUT'], detail=True)
+    @action(methods=["PUT"], detail=True)
     def hide_note(self, request, *args, **kwargs):
         map_data_point = self.get_object()
         map_data_point.processed_by = request.user
         map_data_point.visible = False
-        map_data_point.hidden_reason = request.data.get('hidden_reason', '')
+        map_data_point.hidden_reason = request.data.get("hidden_reason", "")
         map_data_point.save()
-        return Response('OK')
+        return Response("OK")
 
-    @action(methods=['PUT'], detail=True)
+    @action(methods=["PUT"], detail=True)
     def upvote(self, request, *args, **kwargs):
         map_data_point = self.get_object()
         map_data_point.upvotes.get_or_create(user=request.user)
         map_data_point.downvotes.filter(user=request.user).delete()
-        map_data_point = self.get_object() # Reload from db
+        map_data_point = self.get_object()  # Reload from db
         serializer = self.get_serializer(map_data_point)
         return Response(serializer.data)
 
-    @action(methods=['PUT'], detail=True)
+    @action(methods=["PUT"], detail=True)
     def downvote(self, request, *args, **kwargs):
         map_data_point = self.get_object()
         map_data_point.downvotes.get_or_create(user=request.user)
         map_data_point.upvotes.filter(user=request.user).delete()
-        map_data_point = self.get_object() # Reload from db
+        map_data_point = self.get_object()  # Reload from db
         serializer = self.get_serializer(map_data_point)
         return Response(serializer.data)
 
 
 class MapDataPointCommentsViewSet(viewsets.ModelViewSet):
-    queryset = models.MapDataPointComment.objects.all().select_related('user')
+    queryset = models.MapDataPointComment.objects.all().select_related("user")
     permission_classes = [permissions.AllowAny]
     serializer_class = MapDataPointCommentSerializer
+    pagination_class = StandardResultsSetPagination
 
     def get_queryset(self):
         if self.request.user.is_anonymous:
@@ -117,25 +135,27 @@ class MapDataPointCommentsViewSet(viewsets.ModelViewSet):
 
 
 class MapDataPointCommentNotificationsViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = models.MapDataPointCommentNotification.objects\
-        .filter(seen__isnull=True)\
-        .select_related('comment__user')\
-        .order_by('-id')
+    queryset = (
+        models.MapDataPointCommentNotification.objects.filter(seen__isnull=True)
+        .select_related("comment__user")
+        .order_by("-id")
+    )
     permission_classes = [permissions.AllowAny]
     serializer_class = MapDataPointCommentNotificationSerializer
+    pagination_class = StandardResultsSetPagination
 
     def get_queryset(self):
         if self.request.user.is_anonymous:
             return self.queryset.none()
         return self.queryset.filter(user=self.request.user)
 
-    @action(methods=['PUT'], detail=True)
+    @action(methods=["PUT"], detail=True)
     def mark_seen(self, request, *args, **kwargs):
         notification = self.get_object()
         if not notification.seen:
             notification.seen = timezone.now()
         notification.save()
-        return Response('OK')
+        return Response("OK")
 
 
 class MapDataPointsGeoJSON(ListAPIView):
@@ -145,14 +165,19 @@ class MapDataPointsGeoJSON(ListAPIView):
 
     def list(self, request, *args, **kwargs):
         serializer = self.get_serializer()
-        return Response({
-            "type": "FeatureCollection",
-            "features": [{
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [note['lon'], note['lat']]
-                },
-                "properties": serializer.to_representation(note)
-            } for note in self.get_queryset()]
-        })
+        return Response(
+            {
+                "type": "FeatureCollection",
+                "features": [
+                    {
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [note["lon"], note["lat"]],
+                        },
+                        "properties": serializer.to_representation(note),
+                    }
+                    for note in self.get_queryset()
+                ],
+            }
+        )
