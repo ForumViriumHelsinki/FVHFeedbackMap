bde08a3fc41800590aba2f020a140484736c4bd4
Aapo (f021b) Rista <aapris@gmail.com>
2023-02-24 11:33:47 +0200

    Use gis models. Also run black

--- django_server/feedback_map/models/map_data_points.py
+++ django_server/feedback_map/models/map_data_points.py
@@ -4,27 +4,33 @@ from PIL import Image as Img, UnidentifiedImageError, ExifTags
 from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
 from django.core.files import File
-from django.db import models
+from django.contrib.gis.db import models
+
 
 from . import base
 from .base import TimestampedModel
 
 
 def upload_images_to(instance, filename):
-    return f'map_data_points/{instance.id}/{filename}'
+    return f"map_data_points/{instance.id}/{filename}"
 
 
 class Tag(models.Model):
     tag = models.CharField(max_length=64, primary_key=True, unique=True)
-    color = models.CharField(max_length=32, choices=[(c, c) for c in ['blue', 'yellow', 'green']], default='blue')
+    color = models.CharField(
+        max_length=32,
+        choices=[(c, c) for c in ["blue", "yellow", "green"]],
+        default="blue",
+    )
     icon = models.FileField(blank=True, null=True)
     published = models.DateTimeField(blank=True, null=True)
     button_position = models.IntegerField(blank=True, null=True)
 
     def save(self, *args, **kwargs):
         if self.button_position:
-            Tag.objects.filter(button_position=self.button_position, published__isnull=False).exclude(tag=self.tag)\
-                .update(published=None)
+            Tag.objects.filter(
+                button_position=self.button_position, published__isnull=False
+            ).exclude(tag=self.tag).update(published=None)
         return super().save(*args, **kwargs)
 
 
@@ -33,21 +39,40 @@ class MapDataPoint(TimestampedModel):
     lon = models.DecimalField(max_digits=11, decimal_places=8)
     image = models.ImageField(null=True, blank=True, upload_to=upload_images_to)
     comment = models.TextField(blank=True)
-    tags = ArrayField(base_field=models.CharField(max_length=64), default=list, blank=True)
+    tags = ArrayField(
+        base_field=models.CharField(max_length=64), default=list, blank=True
+    )
 
     device_id = models.CharField(max_length=64, blank=True)
 
     created_by = models.ForeignKey(
-        User, null=True, blank=True, on_delete=models.SET_NULL, related_name='created_notes')
+        User,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="created_notes",
+    )
     modified_by = models.ForeignKey(
-        User, null=True, blank=True, on_delete=models.SET_NULL, related_name='modified_notes')
+        User,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="modified_notes",
+    )
 
     processed_by = models.ForeignKey(
-        User, null=True, blank=True, on_delete=models.SET_NULL, related_name='processed_notes')
+        User,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="processed_notes",
+    )
 
     visible = models.BooleanField(default=True)
     hidden_reason = models.TextField(
-        blank=True, help_text="If reviewer decides to hide the note, document reason here.")
+        blank=True,
+        help_text="If reviewer decides to hide the note, document reason here.",
+    )
 
     def __str__(self):
         return self.comment or super().__str__()
@@ -62,7 +87,7 @@ class MapDataPoint(TimestampedModel):
             return super().save(*args, **kwargs)
 
         for orientation in ExifTags.TAGS.keys():
-            if ExifTags.TAGS[orientation] == 'Orientation':
+            if ExifTags.TAGS[orientation] == "Orientation":
                 break
 
         exif = pilImage._getexif()  # noqa
@@ -82,7 +107,7 @@ class MapDataPoint(TimestampedModel):
             pilImage = pilImage.rotate(90, expand=True)
 
         output = BytesIO()
-        pilImage.save(output, format='JPEG', quality=75)
+        pilImage.save(output, format="JPEG", quality=75)
         output.seek(0)
         self.image = File(output, self.image.name)
 
@@ -95,30 +120,47 @@ class MapDataPoint(TimestampedModel):
         from feedback_map.rest.permissions import REVIEWER_GROUP
 
         return User.objects.filter(
-            models.Q(id__in=[self.created_by_id, self.modified_by_id, self.processed_by_id]) |
-            models.Q(map_data_point_comments__map_data_point=self) |
-            models.Q(groups__name=REVIEWER_GROUP)
+            models.Q(
+                id__in=[self.created_by_id, self.modified_by_id, self.processed_by_id]
+            )
+            | models.Q(map_data_point_comments__map_data_point=self)
+            | models.Q(groups__name=REVIEWER_GROUP)
         ).distinct()
 
 
 class MapDataPointUpvote(base.Model):
-    user = models.ForeignKey(User, related_name='map_data_point_upvotes', on_delete=models.CASCADE)
-    map_data_point = models.ForeignKey(MapDataPoint, related_name='upvotes', on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, related_name="map_data_point_upvotes", on_delete=models.CASCADE
+    )
+    map_data_point = models.ForeignKey(
+        MapDataPoint, related_name="upvotes", on_delete=models.CASCADE
+    )
 
 
 class MapDataPointDownvote(base.Model):
-    user = models.ForeignKey(User, related_name='map_data_point_downvotes', on_delete=models.CASCADE)
-    map_data_point = models.ForeignKey(MapDataPoint, related_name='downvotes', on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, related_name="map_data_point_downvotes", on_delete=models.CASCADE
+    )
+    map_data_point = models.ForeignKey(
+        MapDataPoint, related_name="downvotes", on_delete=models.CASCADE
+    )
 
 
 class MapDataPointComment(base.Model):
-    user = models.ForeignKey(User, related_name='map_data_point_comments', on_delete=models.CASCADE, null=True)
-    map_data_point = models.ForeignKey(MapDataPoint, related_name='comments', on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User,
+        related_name="map_data_point_comments",
+        on_delete=models.CASCADE,
+        null=True,
+    )
+    map_data_point = models.ForeignKey(
+        MapDataPoint, related_name="comments", on_delete=models.CASCADE
+    )
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     comment = models.TextField()
 
     class Meta:
-        ordering = ['created_at']
+        ordering = ["created_at"]
 
     def __str__(self):
         return self.comment or super().__str__()
@@ -134,6 +176,10 @@ class MapDataPointComment(base.Model):
 
 
 class MapDataPointCommentNotification(base.Model):
-    comment = models.ForeignKey(MapDataPointComment, related_name='notifications', on_delete=models.CASCADE)
-    user = models.ForeignKey(User, related_name='notifications', on_delete=models.CASCADE)
+    comment = models.ForeignKey(
+        MapDataPointComment, related_name="notifications", on_delete=models.CASCADE
+    )
+    user = models.ForeignKey(
+        User, related_name="notifications", on_delete=models.CASCADE
+    )
     seen = models.DateTimeField(null=True, blank=True, db_index=True)
